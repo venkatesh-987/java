e634b54b39f5489c75d7f0b856b3e020c5c928a4
venkatesh-987 <151707210+venkatesh-987@users.noreply.github.com>
2024-09-05 11:18:33 +0530

    Dowhiledemo.java

---
+++ Dowhiledemo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class Dowhiledemo{
+	public static void main(String args[]){
+		int i=0;
+		do{
+			System.out.println("hello world");
+			i++;
+		}
+		while(i<3);
+	}
+}
+	
