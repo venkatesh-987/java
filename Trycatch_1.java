327b7adddec31b0f0eca919221df7905d5d80fd5
venkatesh-987 <151707210+venkatesh-987@users.noreply.github.com>
2024-10-09 12:07:58 +0530

    Create Trycatch_1.java

---
+++ Trycatch_1.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Trycatch_1
+{
+    public static void main(String args[]){
+        int a=5,b=0,c;
+        c=a/b;
+        System.out.println("value of c is"+c);
+    }
+}
